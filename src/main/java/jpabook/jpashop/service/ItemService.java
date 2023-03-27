7c6262f5a33619f28c97240df65ba06647184930
leeseunghee00 <lsh9246@gmail.com>
2023-03-27 17:58:37 +0900

    feat: 상품 Service 생성

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,33 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional      //readonly로 되어 있기 때문에 이 메소드의 결과값을 저장하기 위해서 사용
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    /**
+     * 조회
+     */
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
