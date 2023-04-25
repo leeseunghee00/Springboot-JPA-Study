de268577b2f7d44833ea17c4ade2ae16c5ef8c1a
leeseunghee00 <lsh9246@gmail.com>
2023-04-25 17:05:55 +0900

    feat: 강제 지연 로딩 설정

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,18 +1,23 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JpashopApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(JpashopApplication.class, args);
+    }
 
-//		Hello hello = new Hello();
-//		hello.setData("hello");
-//		String data = hello.getData();
-//		System.out.println("data = " + data);
-	}
-
+    // 엔티티를 외부에 노출하기 위함 -> 웬만해선 사용하지 않는 것이 좋다 !
+    // 사실 LAZY Loading 때문에 hibernateModule을 사용하는 자체가 좋지 않다.
+    @Bean
+    Hibernate5Module hibernate5Module() {
+        Hibernate5Module hibernate5Module = new Hibernate5Module();
+//        hibernate5Module.configure(Hibernate5Module.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5Module;
+    }
 }
