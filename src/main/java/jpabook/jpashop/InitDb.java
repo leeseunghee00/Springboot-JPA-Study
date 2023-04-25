434c7ebd8c7c2cfd308eb927e320d15499112ff7
leeseunghee00 <lsh9246@gmail.com>
2023-04-25 15:56:35 +0900

    refactor: 중복 코드 메서드로 리팩토링

--- src/main/java/jpabook/jpashop/InitDb.java
+++ src/main/java/jpabook/jpashop/InitDb.java
@@ -18,18 +18,19 @@ import javax.persistence.EntityManager;
  * - SPRING1 BOOK
  * - SPRING2 BOOK
  */
-@Component  //spring이 component 대싱이 된다.
+@Component  //spring이 component 대상이 된다.
 @RequiredArgsConstructor
 public class InitDb {
 
     private final InitService initService;
 
-    // spring life-cycle이 있기 때문에 Transaction 하는 것이 어렵다.
+    // spring life-cycle 이 있기 때문에 Transaction 하는 것이 어렵다.
     // 따라서 dbInit1()의 코드를 한번에 넣는 것이 아니라
-    // 아래와 같이 init으로 따로 빼서 별도의 빈으로 넣어준다.
+    // 아래와 같이 init 으로 따로 빼서 별도의 빈으로 넣어준다.
     @PostConstruct
     public void init() {
         initService.dbInit1();
+        initService.dbInit2();
     }
 
     @Component
@@ -39,33 +40,64 @@ public class InitDb {
         private final EntityManager em;
 
         public void dbInit1() {
-            Member member = new Member();
-            member.setName("userA");
-            member.setAddress(new Address("Seoul", "1", "1234"));
+            Member member = createMember("userA", "Seoul", "1", "1234");
             em.persist(member);
 
-            Book book1 = new Book();
-            book1.setName("JPA1 BOOK");
-            book1.setPrice(10000);
-            book1.setStockQuantity(100);
+            Book book1 = createBook("JPA1 BOOK", 10000, 100);
             em.persist(book1);
 
-            Book book2 = new Book();
-            book2.setName("JPA2 BOOK");
-            book2.setPrice(20000);
-            book2.setStockQuantity(100);
+            Book book2 = createBook("JPA2 BOOK", 20000, 100);
             em.persist(book2);
 
             OrderItem orderItem1 = OrderItem.createOrderItem(book1, 10000, 1);
             OrderItem orderItem2 = OrderItem.createOrderItem(book2, 20000, 2);
 
-            Delivery delivery = new Delivery();
-            delivery.setAddress(member.getAddress());
+            Delivery delivery = createDelivery(member);
 
             //OrderItem... 로 선언했기 때문에 주문상품이 배열로 넘어감
             Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
             em.persist(order);
         }
+
+        public void dbInit2() {
+            Member member = createMember("userB", "Jeju", "2", "5678");
+            em.persist(member);
+
+            Book book1 = createBook("SPRING1 BOOK", 20000, 200);
+            em.persist(book1);
+
+            Book book2 = createBook("SPRING2 BOOK", 40000, 300);
+            em.persist(book2);
+
+            OrderItem orderItem1 = OrderItem.createOrderItem(book1, 20000, 3);
+            OrderItem orderItem2 = OrderItem.createOrderItem(book2, 40000, 4);
+
+            Delivery delivery = createDelivery(member);
+
+            //OrderItem... 로 선언했기 때문에 주문상품이 배열로 넘어감
+            Order order = Order.createOrder(member, delivery, orderItem1, orderItem2);
+            em.persist(order);
+        }
+
+        private static Member createMember(String name, String city, String street, String zipcode) {
+            Member member = new Member();
+            member.setName(name);
+            member.setAddress(new Address(city, street, zipcode));
+            return member;
+        }
+
+        private static Book createBook(String title, int price, int stockQuantity) {
+            Book book = new Book();
+            book.setName(title);
+            book.setPrice(price);
+            book.setStockQuantity(stockQuantity);
+            return book;
+        }
+
+        private static Delivery createDelivery(Member member) {
+            Delivery delivery = new Delivery();
+            delivery.setAddress(member.getAddress());
+            return delivery;
+        }
     }
 }
-
