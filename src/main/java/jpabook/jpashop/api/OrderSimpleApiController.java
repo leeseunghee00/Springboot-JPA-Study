e97a230be0bcaed255767f1ce17de7e4b17f2761
leeseunghee00 <lsh9246@gmail.com>
2023-04-25 17:44:04 +0900

    feat: 엔티티를 DTO로 변환

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToONE(ManyToOne, OneToOne) => 즉, 컬렉션이 아닌 엔티티를 연결해 주는 코드를 작성한다.
@@ -22,7 +27,7 @@ public class OrderSimpleApiController {
     private final OrderRepository orderRepository;
 
     /**
-     * 엔티티 직접 노출 ... 애초에 좋지 않다 !
+     * V1. 엔티티 직접 노출 ... 애초에 좋지 않다 !
      * - Hibernate5Module 모듈 등록 -> LAZY=null 처리
      * - 양방한 관계 문제 발생 -> 한 쪽에 @JsonIgnore 로 설정
      */
@@ -37,5 +42,38 @@ public class OrderSimpleApiController {
         }
 
         return all;
+    }
+
+    /**
+     * V2. 엔티티를 조회해서 DTO로 변환 (fetch join 사용 X)
+     */
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2() {
+        // ORDER 2개
+        // N + 1 -> 1 + 회원 N + 배송 N
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName(); // LAZY 초기화
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress(); // LAZY 초기화
+        }
     }
 }
