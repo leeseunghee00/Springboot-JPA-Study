ad841438961c4085c1f5832fe5a37a01334a76f1
leeseunghee00 <lsh9246@gmail.com>
2023-04-28 17:16:51 +0900

    rename: OrderQuery용 레포지토리명 수정

--- src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -1,4 +1,4 @@
-package jpabook.jpashop.repository.order.sipmlequery;
+package jpabook.jpashop.repository.order.simplequery;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
@@ -15,7 +15,7 @@ public class OrderSimpleQueryRepository {
     private final EntityManager em;
 
     public List<OrderSimpleQueryDto> findOrderDtos() {
-        return em.createQuery("select new jpabook.jpashop.repository.order.sipmlequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+        return em.createQuery("select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
                         " from Order o" +
                         " join o.member m" +
                         " join o.delivery d", OrderSimpleQueryDto.class)
