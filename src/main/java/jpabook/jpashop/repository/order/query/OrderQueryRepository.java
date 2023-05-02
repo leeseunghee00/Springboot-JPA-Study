6c9d80d02a1389a4c04d17e4f74e3c6644d1cdd0
leeseunghee00 <lsh9246@gmail.com>
2023-05-02 14:05:35 +0900

    feat: 주문 조회 V6 - 플랫 데이터 최적화

--- src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
+++ src/main/java/jpabook/jpashop/repository/order/query/OrderQueryRepository.java
@@ -99,4 +99,17 @@ public class OrderQueryRepository {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * DB 에서 쿼리를 한방에 가져오기
+     */
+    public List<OrderFlatDto> findAllByDto_flat() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.query.OrderFlatDto(o.id, m.name, o.orderDate, o.status, d.address, i.name, oi.orderPrice, oi.count)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d" +
+                                " join o.orderItems oi" +
+                                " join oi.item i", OrderFlatDto.class)
+                .getResultList();
+    }
 }
